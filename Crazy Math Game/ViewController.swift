4acae64f4b5f2b05118c1f1a5fd87a48af63015c
Ербол Каршыга <erbolkarsyga@erbols-mac.local>
2016-06-08 01:02:16 -0700

    Initial Commit

---
+++ Crazy Math Game/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Crazy Math Game
+//
+//  Created by Ербол Каршыга on 6/8/16.
+//  Copyright © 2016 Ербол Каршыга. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Crazy Math GameTests/Crazy_Math_GameTests.swift
@@ -0,0 +1,36 @@
+//
+//  Crazy_Math_GameTests.swift
+//  Crazy Math GameTests
+//
+//  Created by Ербол Каршыга on 6/8/16.
+//  Copyright © 2016 Ербол Каршыга. All rights reserved.
+//
+
+import XCTest
+@testable import Crazy_Math_Game
+
+class Crazy_Math_GameTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
